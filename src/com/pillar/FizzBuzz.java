4226655e856d960b57141392613fbd81f11cc29f
riley-moss <rileypmoss@gmail.com>
2019-02-13 18:28:50 -0600

    Finished buzz portion of FizzBuzz

--- src/com/pillar/FizzBuzz.java
+++ src/com/pillar/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
 	public String fizz(Integer inputNumber) {
 		if(inputNumber % 3 == 0)
 			return "fizz";
+		else if(inputNumber % 5 == 0)
+			return "buzz";
 		return inputNumber.toString();
 	}
 
