a9885f467a3d704fd5f9f2126da9f7f94c1a72df
riley-moss <rileypmoss@gmail.com>
2019-02-13 18:26:31 -0600

    Refactored FizzBuzz

--- test/com/pillar/FizzBuzzTest.java
+++ test/com/pillar/FizzBuzzTest.java
@@ -1,24 +1,30 @@
 package com.pillar;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class FizzBuzzTest {
+	
+	FizzBuzz fizzBuzz;
+	
+	@Before
+	public void setUp() {
+		fizzBuzz = new FizzBuzz();
+	}
+	
 	@Test
 	public void whenFizzBuzzIsPassedNumberItReturnsThatNumber() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("1", fizzBuzz.fizz(1));
 	}
 	
 	@Test
 	public void whenFizzBuzzIsPassedAtwoItReturnsATwo() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("2", fizzBuzz.fizz(2));
 	}
 	
 	@Test
 	public void whenFizzBuzzIsPassedMultipleOfThreeItReturnsFizz() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("fizz", fizzBuzz.fizz(3));
 		assertEquals("fizz", fizzBuzz.fizz(6));
 	}
